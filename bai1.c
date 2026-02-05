c12b9013cf170120164f9ecf4ff447844868d36c
tienphuclu2088-eng <tienphuclu2088@gmail.com>
2026-02-05 07:38:30 +0700

    PHUC TIEN NỘP LAB5

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Lưu Phúc Tiến]
+ * MSSV:      [PS48200]
+ * Lớp:       [COM108-CS21301]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÌM GIÁ TRỊ LỚN NHẤT TRONG 3 SỐ  
@@ -11,19 +11,37 @@
 //  giá trị lớn nhất giữa 3 số 
 
 #include <stdio.h>
-
-// //Tạo hàm 
-// int tenHam(so1, so2, so3){ 
-// int max; 
-// //Cách giải để tìm ra số lớn nhất 
-// return max; 
-// } 
+int timMax(int a, int b, int c) {
+    int max = a;
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+    return max;
+}
 
 int main() {
-
-    // //Gọi hàm trong hàm main 
-    // //ví dụ 
-    // int soMax = temHam(3,5,10)
+    int a, b, c;
+    
+    // Nhập 3 số từ bàn phím
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &a);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &b);
+    printf("Nhap so thu ba: ");
+    scanf("%d", &c);
+    
+    // Gọi hàm tìm max
+    int max = timMax(a, b, c);
+    
+    // Xuất kết quả
+    printf("So lon nhat trong 3 so %d, %d, %d la: %d\n", a, b, c, max);
     
     return 0;
 }
+
+
+    
+    
